7b64fb3f671078c0bcc6fc9f80739707c3c988bd
Esterling Accime <esterling@brainbytescreative.com>
2018-11-11 15:06:54 -0500

    adding twitter and facebook sharing capability

--- jou-2/random-quote-generator-project/js/app.js
+++ jou-2/random-quote-generator-project/js/app.js
@@ -1,26 +1,51 @@
+let activeQuote = '';
 
+
+// Displaying random quotes
 const render = (data) => {
-    console.log('Data: ', data);
+    const quote = data.quote;
 
-    $('#quote').html(data.quote);
+    activeQuote = quote
+    $('#quote').html(quote);
     $('.blockquote-footer').html(data.author);
-    // alert('Successfully made!');
 };
 
 
+// Fetching random quote
 const fetchRandomQuote = () => {
+
+   
     $.ajax({
         url: 'https://random-quote-creole.herokuapp.com/random',
         method: 'GET'
     })
         .done((data) => render(data))
-        .fail(() => {
-            alert('Ooops, something happened!');
-        });
+        .fail(() => alert('Ooops, something happened!'));
 };
 
 fetchRandomQuote();
 
-$('#new-quote').on('click', () => {
-    fetchRandomQuote();
+// Getting new quote
+$('#new-quote').on('click', () => fetchRandomQuote());
+
+$('.socialButton').on('click', function() {
+    console.log('Testing');
+    const self = $(this);
+    const socialMedia = self.html().toLowerCase();
+
+    if (socialMedia.toLowerCase().includes('facebook')) {
+        const webUrl = 'https://www.facebook.com/sharer/sharer.php?';
+        const u = 'github.com';
+        open(`${webUrl}u=${u}&quote=${activeQuote}`, 'pop', 'width=600, height=400, scrollbars=no');
+        return false;
+    }
+
+    const url = `https://twitter.com/intent/tweet?text=${activeQuote}&hashtags=haitiCodingWorkshop`;
+    open(url, 'pop', 'width=600, height=400, scrollbars=no');
 });
+
+
+// Showing the loading spinner
+$(document)
+    .ajaxSend(() => $('.loader').addClass('loading'))
+    .ajaxStop(() => $('.loader').removeClass('loading'));
